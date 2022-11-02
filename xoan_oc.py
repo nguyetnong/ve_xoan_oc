c171c229a12061baac985e6df2c14a61092548eb
nguyetnong <nguyetnong71099@gmail.com>
2022-11-02 22:07:38 +0700

    xoan oc 2

--- xoan_oc.py
+++ xoan_oc.py
@@ -1,10 +1,18 @@
 import turtle
 
-
+mau_sac =['red','green']
+mau_sac[0]
 d=200
 buoc_di=0
+turtle.pensize(5)
+cnt =0
 while (turtle.distance(0,0)<d):
+    if cnt % 2 == 0:
+        turtle.color(mau_sac[0])
+    else:
+        turtle.color(mau_sac[1])
     buoc_di+=0.3
     turtle.forward(buoc_di)
     turtle.left(30)
+    cnt = cnt +1
 turtle.done()
